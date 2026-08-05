10c56bf00e8210d81b98e5a97ffb28760f2039cb
Farrukh Rashid <44774880+farrukhrashid1997@users.noreply.github.com>
2026-08-05 11:09:56 -0300

    added lakebase.py

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
-import psycopg2
 import streamlit as st
-from databricks.sdk import WorkspaceClient
+
+import lakebase
 
 st.title("Lakebase Connection Test")
 
 try:
-    w = WorkspaceClient()  # auto-authenticates using DATABRICKS_CLIENT_ID/SECRET from env
-    lakebase_url = w.dbutils.secrets.get(scope="database", key="lakebase-url")
-    
-    conn = psycopg2.connect(lakebase_url)
-    cur = conn.cursor()
-    cur.execute("SELECT version();")
-    version = cur.fetchone()
+    result = lakebase.run_query("SELECT version();")
     st.success("✅ Connected to Lakebase!")
-    st.write("Postgres version:", version[0])
-    cur.close()
-    conn.close()
+    st.write(result)
 except Exception as e:
     st.error(f"Failed: {e}")
+    st.exception(e)
